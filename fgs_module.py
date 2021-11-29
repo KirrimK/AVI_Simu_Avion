0e8bcf6f63805de89d6f752eb311402a63823aea
KirrimK <KirrimK@users.noreply.github.com>
2021-11-29 13:23:05 +0100

    basic ivy calls

--- fgs_module.py
+++ fgs_module.py
@@ -1,1 +1,13 @@
- 
+# FGS_module
+
+from ivy.std_api import *
+import time
+IvyInit("FGS", "Ready")
+IvyStart("10.1.127.255:2010") #IP à changer
+time.sleep(1.0)
+
+#IvySendMsg("")
+#IvyBindMsg(callback, "regex")
+
+#def generic_callback(sender, *data):
+#    pass
