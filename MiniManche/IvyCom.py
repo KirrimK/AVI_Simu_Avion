49a3e5126da6f925f6f108980939bf6298f3b0eb
Victor755431 <azarbidu54@gmail.com>
2021-12-03 12:44:00 +0100

    Ajout du message de limittes

--- MiniManche/IvyCom.py
+++ MiniManche/IvyCom.py
@@ -9,6 +9,7 @@ class IvyRadio ():
         self.vecteurDEtat = "^StateVector x=(.+) y=(.+) z=(.+) Vp=(.+) fpa=(.+) psi=(.+) phi=(.+)$"
         self.commandeAutoPilote = "^AutoPilot nx=(.+) nz=(.+) rollRate=(.+)$"
         #Messages à envoyer : 
+        self.infoLimites = "MM Limites vMin={} vMax={} phiLim={} nxMin={} nxMax={} nzMin={} nzMax={} pLim={}"
         self.commandeVitesse = "MM Vc={}"
         self.FCUAP = "FCUAP {}"
         self.commandeModeleNx = "APNxControl nx={}"
@@ -20,28 +21,38 @@ class IvyRadio ():
         IvyBindMsg (self.onRcvAPCommand, self.commandeAutoPilote)
 
     def startRadio (self):
+        """Fonction À APPELER IMPÉRATIVEMENT AU LANCEMENT DU PROGRAMME. Démarre la radio."""
         IvyStart (self.bus)
+
     def stopRadio (self):
+        """Fonction À APPELER IMPÉRATIVEMENT À L'ARRÊT DU PROGRAMME. Enlève la radio du bus Ivy."""
         IvyStop()
+
     def sendMessage (self, message):
+        """Fonction prenant une chaine de caractères et l'envoyant sur le bus Ivy """
         IvySendMsg(message)
 
         #Réactions aux messages
     def onBoutonAPPush (self,sender):
+        """Fonction appelée par un callback d'un message de l'interface.
+        Broadcast la valeur allumée ou éteinte du pilote automatique."""
         #Traitement
         self.sendAPState()
     def onRcvStateVector (self,sender,x,y,z,Vp,fpa,psi,phi):
+        """Fonction appelée par un callback d'un message de l'interface.
+        Les valeurs sont utilisées pour calculer les limites de commande."""
         pass
     def onRcvAPCommand (self,sender,nx, nz, p):
-        """Fonction appelée par une callback de message pilote automatique"""
+        """Fonction appelée par une callback de message pilote automatique.
+        Appelle l'envoie des valeurs approuvées par les limites de l'avion."""
         pass
         #Envoi de messages
     def sendSpeedCommand (self, Vc):
-        """Envoie une commande de vitesse managée à l'autilote."""
+        """Envoie une commande de vitesse managée à l'auto pilote."""
         self.sendMessage
 
     def sendAPState(self):
-        """Envoie l'état de l'autopilote."""
+        """Envoie l'état de l'auto pilote."""
         état = getAPState ()
         self.sendMessage(self.FCUAP.format(état))
 
@@ -50,3 +61,6 @@ class IvyRadio ():
         self.sendMessage (self.commandeModeleNx.format(nx))
         self.sendMessage (self.commandeModeleNz.format(nz))
         self.sendMessage (self.commandeModelep.format (p))
+
+    def sendLimitsInfo (self, vMin, vMax, phiLim, nxMin, nxMax, nzMin, nzMax, pLim):
+        self.sendMessage(self.infoLimites.format(vMin, vMax, phiLim, nxMin, nxMax, nzMin, nzMax, pLim))
