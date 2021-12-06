eff27016051b5b9939eeaf3053cae32f12aac40f
quentin-sgn <quentin.signe@icloud.com>
2021-12-06 16:15:37 +0100

    update manche

--- MiniManche/windowQt.py
+++ MiniManche/windowQt.py
@@ -10,7 +10,7 @@ class Window(QMainWindow):
         self.setWindowTitle("Contrôles des sufarces de vol")
         self.radio = 
         self.setupSliders ()
-        self.axis0Brut = 0
-        self.axis1Brut = 1
+        self.pBrut = 0
+        self.nzBrut = 0
     def setupSliders (self):
         pass
