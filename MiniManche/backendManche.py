c1051724cf0350a41e363c96592e17a3d76a4968
quentin-sgn <quentin.signe@icloud.com>
2021-12-06 16:57:07 +0100

    Update backendManche.py

--- MiniManche/backendManche.py
+++ MiniManche/backendManche.py
@@ -30,7 +30,7 @@ class TextPrint(object):
 
 
 class MancheRadio():
-    def _init_(self, window):
+    def __init__(self, window):
         self.window = window
         self.done = False
         self.VecteurDEtatSignal.connect(self.onSignalRadio)
