48878538cf5f305ecebccb5a8f7d7a15022faa34
AnneLaureVergnes <75971988+AnneLaureVergnes@users.noreply.github.com>
2021-12-08 16:15:30 +0100

    update

--- MiniManche/limites.py
+++ MiniManche/limites.py
@@ -24,7 +24,7 @@ R=278.05
 T0=288.16 #en Kelvin
 CI=0.35 #Cost Index
 
-#mamamamama
+
 
 
 class Avion:
@@ -43,64 +43,64 @@ class Avion:
 		self.PA=True 
 
 
-def vitesse_limite(self.alt,self.vitesse_lim):
+def vitesse_limite(avion):
 	#dépend de l'altitude et de la configuration physique de l'avion
 	limite_alt=[10000*FT2M,24600*FT2M]
 	vitesse_lim=0
-	if self.train==False and self.config_physique==0:
-		if self.alt<limite_alt[0]:
-			self.Mach=False
+	if avion.train==False and avion.config_physique==0:
+		if avion.alt<limite_alt[0]:
+			avion.Mach=False
 			vitesse_lim=250*KT2MS
-		if limite_alt[0]<self.alt<limite_alt[1]:
-			self.Mach=False
+		if limite_alt[0]<avion.alt<limite_alt[1]:
+			avion.Mach=False
 			vitesse_lim=350*KT2MS
-		if self.alt>limite_alt[1]:
-			self.Mach=True
+		if avion.alt>limite_alt[1]:
+			avion.Mach=True
 			#Mach maximal=0.82
 			#calcul du Mach
-			T=T0-6.5*(self.alt/1000)
+			T=T0-6.5*(avion.alt/1000)
 			a=sqrt(gamma_constante*R*T)
 			vitesse_lim=0.82*a
-	if self.train==True and self.config_physique==0:
-		self.Mach=False
+	if avion.train==True and avion.config_physique==0:
+		avion.Mach=False
 		vitesse_lim=280*KT2MS
 
-	if self.config_physique==1:
-		self.Mach=False
+	if avion.config_physique==1:
+		avion.Mach=False
 		vitesse_lim=230*KT2MS
-	if self.config_physique==2:
-		self.Mach=False
+	if avion.config_physique==2:
+		avion.Mach=False
 		vitesse_lim=200*KT2MS
-	if self.config_physique==3:
-		self.Mach=False
+	if avion.config_physique==3:
+		avion.Mach=False
 		vitesse_lim=185*KT2MS
-	if self.config_physique==4:
-		self.Mach=False
+	if avion.config_physique==4:
+		avion.Mach=False
 		vitesse_lim=177*KT2MS
 		
-	return self.Mach,vitesse_lim
+	return avion.Mach,vitesse_lim
 
-def nz_limites(self.train,self.config_physique):
+def nz_limites(avion):
 	#les limitations du facteur de charge dépendent de la configuration physique de l'avion
 	#tester si ces limites fonctionnent bien avec les trains sortis
 	nz_lim=[0,0] #liste avec les valeurs limites de nz
-	if self.train==False:
-		if self.config_physique==0:
+	if avion.train==False:
+		if avion.config_physique==0:
 		 	nz_lim=[-1,2.5]
 		else:
 			nz_lim=[0,2]
-	if self.train=True:
-		if self.config_physique==0:
+	if avion.train==True:
+		if avion.config_physique==0:
 			nz_lim=[0,2.5]
 		else:
 			nz_lim=[0,1.5]
 	return nz_lim
 	
-def nx_limites(self.train,self.config_physique):
+def nx_limites(avion):
 	#il va falloir tester si ces limites ne sont pas trop restrictives, rien trouvé pour l'instant dans la doc et on a trouvé ça après discution avec M.Rouillard
 	nx_lim=[0,0]
-	if self.train==False:
-		if self.config_physique==0:
+	if avion.train==False:
+		if avion.config_physique==0:
 			nx_lim=[-1.5,1.5]
 		else:
 			nx_lim=[-1,1]
@@ -112,19 +112,27 @@ def p_lim():
 	p_lim=15*DEG2RAD #pour l'exprimer en radian/s
 	return p_lim
 	
-def phi_lim(self.PA):
+def phi_lim(avion):
 	phi_lim=[0,0]
-	if abs(self.phi)>33*DEG2RAD:
-		self.PA=False 
-	if self.PA==False:
+	if abs(avion.phi)>33*DEG2RAD:
+		avion.PA=False 
+	if avion.PA==False:
 		phi_lim=[-67*DEG2RAD,67*DEG2RAD]
-	if self.PA==True:
+	if avion.PA==True:
 		phi_lim=[-33*DEG2RAD,33*DEG2RAD]
 	return phi_lim
 	
-#limite en gamma 25,30 
+#limite en gamma 25,30
 
-
+def limite_gamma(avion):
+        gamma_lim=[0,0]
+        if abs(avion.gamma)>25*DEG2RAD:
+                avion.PA=False
+        if avion.PA==False:
+                gamma_lim=[-30*DEG2RAD,30*DEG2RAD]
+        else:
+                gamma_lim=[-25*DEG2RAD,25*DEG2RAD]
+        return gamma_lim
 	
 		
 			
