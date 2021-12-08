dac00d567416cff7d939257b6fb3b3f814aaae4d
quentin-sgn <quentin.signe@icloud.com>
2021-12-08 16:19:57 +0100

    affichage pygame

--- MiniManche/backendManche.py
+++ MiniManche/backendManche.py
@@ -1,10 +1,8 @@
-from PyQt5.QtWidgets import QWidget
 import pygame
 import threading
 
 BLACK = pygame.Color('black')
 WHITE = pygame.Color('white')
-
 
 
 class TextPrint(object):
@@ -45,18 +43,18 @@ class MancheRadio():
         pygame.init()
 
         # Définit la taille de la fenetre (largeur, hauteur).
-        #screen = pygame.display.set_mode((500, 700))
+        screen = pygame.display.set_mode((500, 700))
 
-        #pygame.display.set_caption("Manche")
+        pygame.display.set_caption("Manche")
 
         # Permet de savoir la vitesse de mise à jour des images de la fenetre.
-        #clock = pygame.time.Clock()
+        clock = pygame.time.Clock()
 
         # Initialise le joystick.
         pygame.joystick.init()
 
         # Lancement de l'affichage des données.
-        #textPrint = TextPrint()
+        textPrint = TextPrint()
 
         # -------- boucle principale -----------
         while not self.done:
@@ -80,14 +78,14 @@ class MancheRadio():
             #
             # First, clear the screen to white. Don't put other drawing commands
             # above this, or they will be erased with this command.
-            #screen.fill(WHITE)
-            #textPrint.reset()
+            screen.fill(WHITE)
+            textPrint.reset()
 
             # Get count of joysticks.
             joystick_count = pygame.joystick.get_count()
 
-            #textPrint.tprint(screen, "Number of joysticks: {}".format(joystick_count))
-            #textPrint.indent()
+            textPrint.tprint(screen, "Number of joysticks: {}".format(joystick_count))
+            textPrint.indent()
 
             # For each joystick:
             for i in range(joystick_count):
@@ -99,12 +97,12 @@ class MancheRadio():
                 except AttributeError:
                     # get_instance_id() is an SDL2 method
                     jid = joystick.get_id()
-                #textPrint.tprint(screen, "Joystick {}".format(jid))
-                #textPrint.indent()
+                textPrint.tprint(screen, "Joystick {}".format(jid))
+                textPrint.indent()
 
                 # Get the name from the OS for the controller/joystick.
                 name = joystick.get_name()
-                #textPrint.tprint(screen, "Joystick name: {}".format(name))
+                textPrint.tprint(screen, "Joystick name: {}".format(name))
 
                 try:
                     guid = joystick.get_guid()
@@ -113,40 +111,39 @@ class MancheRadio():
                     pass
                 else:
                     pass
-                    #textPrint.tprint(screen, "GUID: {}".format(guid))
+                    textPrint.tprint(screen, "GUID: {}".format(guid))
 
                 # Usually axis run in pairs, up/down for one, and left/right for
                 # the other.
                 axes = joystick.get_numaxes()
-                #textPrint.tprint(screen, "Number of axes: {}".format(axes))
-                #textPrint.indent()
+                textPrint.tprint(screen, "Number of axes: {}".format(axes))
+                textPrint.indent()
 
                 self.window.pBrut = joystick.get_axis(0)
-                #textPrint.tprint(screen, "Axis 0 value: {:>6.3f}".format(joystick.get_axis(0)))
+                textPrint.tprint(screen, "Axis 0 value: {:>6.3f}".format(joystick.get_axis(0)))
                 self.window.nzBrut = joystick.get_axis(1)
-                #textPrint.tprint(screen, "Axis 1 value: {:>6.3f}".format(joystick.get_axis(1)))
-                #textPrint.unindent()
+                textPrint.tprint(screen, "Axis 1 value: {:>6.3f}".format(joystick.get_axis(1)))
+                textPrint.unindent()
 
                 buttons = joystick.get_numbuttons()
-                #textPrint.tprint(screen, "Number of buttons: {}".format(buttons))
-                #textPrint.indent()
+                textPrint.tprint(screen, "Number of buttons: {}".format(buttons))
+                textPrint.indent()
 
                 for i in range(buttons):
                     button = joystick.get_button(i)
-                    #textPrint.tprint(screen,
-                                    #"Button {:>2} value: {}".format(i, button))
-                #textPrint.unindent()
-                #textPrint.unindent()
+                    textPrint.tprint(screen,"Button {:>2} value: {}".format(i, button))
+                textPrint.unindent()
+                textPrint.unindent()
 
             #
             # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
             #
 
             # Go ahead and update the screen with what we've drawn.
-            #pygame.display.flip()
+            pygame.display.flip()
 
             # Limit to 20 frames per second.
-            #clock.tick(20)
+            clock.tick(20)
 
         # Close the window and quit.
         # If you forget this line, the program will 'hang'
